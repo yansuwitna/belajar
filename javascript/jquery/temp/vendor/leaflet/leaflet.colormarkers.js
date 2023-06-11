f18dcc5dadcec91e3032bab79085d22edcef58ca
Yansuwitna <yansuwitna@gmail.com>
2023-06-11 22:28:13 +0800

    unbk

---
+++ javascript/jquery/temp/vendor/leaflet/leaflet.colormarkers.js
@@ -0,0 +1,71 @@
+var blueIcon = new L.Icon({
+	iconUrl: baseURL + 'vendor/leaflet/img/marker-icon-2x-blue.png',
+	shadowUrl: baseURL + 'vendor/leaflet/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+	iconUrl: baseURL + 'vendor/leaflet/img/marker-icon-2x-red.png',
+	shadowUrl: baseURL + 'vendor/leaflet/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greenIcon = new L.Icon({
+	iconUrl: baseURL + 'vendor/leaflet/img/marker-icon-2x-green.png',
+	shadowUrl: baseURL + 'vendor/leaflet/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var orangeIcon = new L.Icon({
+	iconUrl: baseURL + 'vendor/leaflet/img/marker-icon-2x-orange.png',
+	shadowUrl: baseURL + 'vendor/leaflet/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var yellowIcon = new L.Icon({
+	iconUrl: baseURL + 'vendor/leaflet/img/marker-icon-2x-yellow.png',
+	shadowUrl: baseURL + 'vendor/leaflet/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var violetIcon = new L.Icon({
+	iconUrl: baseURL + 'vendor/leaflet/img/marker-icon-2x-violet.png',
+	shadowUrl: baseURL + 'vendor/leaflet/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greyIcon = new L.Icon({
+	iconUrl: baseURL + 'vendor/leaflet/img/marker-icon-2x-grey.png',
+	shadowUrl: baseURL + 'vendor/leaflet/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var blackIcon = new L.Icon({
+	iconUrl: baseURL + 'vendor/leaflet/img/marker-icon-2x-black.png',
+	shadowUrl: baseURL + 'vendor/leaflet/img/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
